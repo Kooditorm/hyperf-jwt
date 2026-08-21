afb6e481f1ca756d30eb8788aca77c1a280347a7
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-08-21 17:04:58 +0800

    更新代码

---
+++ src/Auth/Access/GateManager.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Kooditorm/hyperf-jwt.
+ *
+ * @link     https://github.com/Kooditorm/hyperf-jwt
+ * @contact  oswin.hu@gmail.com
+ * @license  https://github.com/Kooditorm/hyperf-jwt/blob/master/LICENSE
+ */
+
+namespace Kooditorm\Hyperf\Auth\Access;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Di\Annotation\AnnotationCollector;
+use Kooditorm\Hyperf\Auth\Contracts\Access\GateManagerInterface;
+
+class GateManager implements GateManagerInterface
+{
+
+}
