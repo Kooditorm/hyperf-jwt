cb4992c64a7a03ec7dd4dae36e828285f284acb1
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-08-21 17:02:23 +0800

    更新代码

---
+++ src/Auth/Middlewares/AbstractAuthenticateMiddleware.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Kooditorm/hyperf-jwt.
+ *
+ * @link     https://github.com/Kooditorm/hyperf-jwt
+ * @contact  oswin.hu@gmail.com
+ * @license  https://github.com/Kooditorm/hyperf-jwt/blob/master/LICENSE
+ */
+
+namespace Kooditorm\Hyperf\Auth\Middlewares;
+
+use Kooditorm\Hyperf\Auth\Contracts\AuthManagerInterface;
+use Kooditorm\Hyperf\Auth\Exceptions\AuthenticationException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+class AbstractAuthenticateMiddleware extends MiddlewareInterface
+{
+    /**
+     * The authentication factory instance.
+     *
+     * @var \Kooditorm\Hyperf\Auth\Contracts\AuthManagerInterface
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     */
+    public function __construct(AuthManagerInterface $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Kooditorm\Hyperf\Auth\Exceptions\AuthenticationException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->authenticate($request, $this->guards());
+        return $handler->handle($request);
+    }
+
+    /**
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @throws \Kooditorm\Hyperf\Auth\Exceptions\AuthenticationException
+     */
+    protected function authenticate(ServerRequestInterface $request, array $guards): void
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                $this->auth->shouldUse($guard);
+                return;
+            }
+        }
+
+        ! $this->passable() and $this->unauthenticated($request, $guards);
+    }
+
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @throws \Kooditorm\Hyperf\Auth\Exceptions\AuthenticationException
+     */
+    protected function unauthenticated(ServerRequestInterface $request, array $guards): void
+    {
+        throw new AuthenticationException(
+            'Unauthenticated.',
+            $guards,
+            $this->redirectTo($request)
+        );
+    }
+
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(ServerRequestInterface $request): ?string
+    {
+        return null;
+    }
+
+    /**
+     * Determines whether an unauthenticated user can pass the auth guard.
+     * In general, this should be return FALSE. However in some special cases,
+     * return TRUE would be useful.
+     */
+    protected function passable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get guard names.
+     *
+     * @return string[]
+     */
+    abstract protected function guards(): array;
+}
