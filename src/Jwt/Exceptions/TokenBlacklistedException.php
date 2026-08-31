19201745b2fcbb0b651fcb04e15660234db9de52
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-08-31 12:23:21 +0800

    更新代码

--- src/Jwt/Exceptions/TokenBlacklistedException.php
+++ src/Jwt/Exceptions/TokenBlacklistedException.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 
 namespace Kooditorm\Hyperf\Jwt\Exceptions;
 
-class TokenBlacklistedException extends TokenException
+class TokenBlacklistedException extends JwtException
 {
 
 }
