f6ae05e39e0dfdd809029950384b3a14ff96d8a5
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-09-11 17:32:59 +0800

    更新代码

--- src/Auth/Annotations/Auth.php
+++ src/Auth/Annotations/Auth.php
@@ -20,7 +20,7 @@ use Hyperf\Di\Annotation\AbstractAnnotation;
 class Auth extends AbstractAnnotation
 {
     /**
-     * @var string[]
+     * @var array
      */
     public array $guards = [];
 
@@ -29,17 +29,17 @@ class Auth extends AbstractAnnotation
      */
     public bool $passable;
 
-    public function __construct($value = null)
+    public function __construct(array|string $guards, bool $passable = false)
     {
-        if (isset($value['value'])) {
-            if (!empty($value['value']) && is_array($value['value'])){
-                $this->guards = array_unique($value['value']);
+        if (!empty($guards)) {
+            if (is_array($guards)) {
+                $this->guards = array_unique($guards);
             } else {
-                $this->guards = [$value['value']];
+                $this->guards = [$guards];
             }
         }
         if (isset($value['passable'])) {
-            $this->passable = (bool) $value['passable'];
+            $this->passable = $passable;
         }
     }
 }
